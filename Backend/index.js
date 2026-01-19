bc62a820ab3bf69b4436561db67eac8b827930cb
Mayank Hansraj <mayankhansraj12@gmail.com>
2026-01-19 15:50:55 +0530

    fir

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
