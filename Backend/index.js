653f1a014da7bfb5ac575e34038fb22042240d4d
Mayank Hansraj <mayankhansraj12@gmail.com>
2026-01-19 18:23:08 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-app.use(cors({ origin: "https://authentication-frontend-0lo3.onrender.com", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
