2408ff120ddffab8514a380ffc177040d835de58
Mayank Hansraj <mayankhansraj12@gmail.com>
2026-01-19 18:51:50 +0530

    Update authStore.js

--- Frontend/src/store/authStore.js
+++ Frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import {create} from 'zustand';
 import axios from 'axios';
 
-const API_URL = "https://authentication-backend-ycsy.onrender.com"
+const API_URL = "http://localhost:5000/api/auth"
 
 axios.defaults.withCredentials = true;
 
