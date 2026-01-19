88c533ceb0d0d5b39d4a510e111734a56c2b9e94
Mayank Hansraj <mayankhansraj12@gmail.com>
2026-01-19 15:28:09 +0530

    first commit

---
+++ Backend/mailtrap/mailtrap.config.js
@@ -0,0 +1,17 @@
+import { MailtrapClient } from 'mailtrap';
+import dotnev from "dotenv";
+
+dotnev.config();
+
+const TOKEN = process.env.MAILTRAP_TOKEN;
+const ENDPOINT = process.env.MAILTRAP_ENDPOINT;
+
+export const mailtrapClient = new MailtrapClient({
+  token: TOKEN,
+});
+
+export const sender = {
+  email: "hello@demomailtrap.co",
+  name: "Mailtrap Test",
+};
+
