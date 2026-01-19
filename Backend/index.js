e0ac0877177ba66899bd538ebd9a4139c7f46872
mayankhansraj12 <mayankhansraj12@gmail.com>
2026-01-19 16:24:48 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+//app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: "https://authentication-frontend-0lo3.onrender.com", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
